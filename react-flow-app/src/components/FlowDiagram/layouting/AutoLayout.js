ae89fc8dd710d41303f5719b5c356d74dd7554d6
ryan3971 <tyrrellr@mcmaster.ca>
2024-08-28 22:25:50 -0400

    Fixed issue with nodes being placed too close together. Was not getting the width and height so it was falling back on the defaults

--- react-flow-app/src/components/FlowDiagram/layouting/AutoLayout.js
+++ react-flow-app/src/components/FlowDiagram/layouting/AutoLayout.js
@@ -6,17 +6,19 @@ const layoutOptions = {
 	"elk.layered.spacing.edgeNodeBetweenLayers": "75",
 	"elk.spacing.nodeNode": "100",
 	"elk.spacing.edgeNode": "100",
+	"elk.layered.spacing.nodeNodeBetweenLayers": "100",
+
 	"elk.layered.nodePlacement.strategy": "SIMPLE",
+	"elk.portConstraints": "FIXED_ORDER", // Ensures ports have defined sides
+
 
 	//"elk.spacing.edgeEdge": "100",
 
-	//"elk.layered.spacing.nodeNodeBetweenLayers": 50,
 	//"elk.layered.spacing.nodeNode": 30,
-	// "elk.layered.layering.strategy": "INTERACTIVE",
+	//"elk.layered.layering.strategy": "INTERACTIVE",
 	//"elk.layered.crossingMinimization.strategy": "LAYER_SWEEP",
-	// "elk.edgeRouting": "ORTHOGONAL",
+	//"elk.edgeRouting": "ORTHOGONAL",
 	// "elk.hierarchyHandling": "INCLUDE_CHILDREN",
-	//"elk.portConstraints": "FIXED_ORDER", // Ensures ports have defined sides
 	//"elk.layered.nodePlacement.bk.edgeCrossing.minimize": "true", // Minimize edge crossings
 };
 
@@ -24,6 +26,7 @@ const elk = new ELK();
 
 // Function to layout the nodes using ELK
 export const getLayoutedNodes = async (nodes, edges) => {
+	console.log("Autolayouting Nodes: ", nodes);
 	const graph = {
 		id: "root",
 		layoutOptions,
@@ -47,18 +50,9 @@ export const getLayoutedNodes = async (nodes, edges) => {
 
 			return {
 				id: node.id,
-				width: node.width ?? 150,
-				height: node.height ?? 50,
-				properties: {
-					"org.eclipse.elk.portConstraints": "FIXED_ORDER",
-				},
-				ports: [
-					{
-						id: `${node.id}`, // Ensure the main port has a unique ID and side assignment
-					},
-					...targetPorts,
-					...sourcePorts,
-				],
+				width: node.measured.width ?? 200,
+				height: node.measured.height ?? 100,
+				ports: [...targetPorts, ...sourcePorts],
 			};
 		}),
 
@@ -73,7 +67,7 @@ export const getLayoutedNodes = async (nodes, edges) => {
 
 	const layoutedNodes = nodes.map((node) => {
 		const layoutedNode = layoutedGraph.children?.find((layoutedGraphNode) => layoutedGraphNode.id === node.id);
-
+		console.log("End of Auto Layouted Node: ", layoutedNode);
 		return {
 			...node,
 			position: {
